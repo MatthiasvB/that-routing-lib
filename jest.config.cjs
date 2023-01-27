2e28ed3099ea2b56e77ead63366bb8e5e84671fb
Matthias vom Bruch <matthias.vombruch@gmx.de>
2023-01-27 11:10:07 +0100

    fixed tests running built files

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
+  modulePathIgnorePatterns: ["/lib/"],
   transform: {
     // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
     // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
